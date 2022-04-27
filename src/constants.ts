12870b2afb8c561b18ebb388e9bbbc7b9bd89fbe
nastari <48503371+nastari@users.noreply.github.com>
2022-04-26 21:20:29 -0300

    addresses

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,18 @@
 import JSBI from 'jsbi'
 
+export const FACTORY_ADDRESS_UNI_1 = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 export const FACTORY_ADDRESS_56 = '0x70EB89A8d61734697bf237309430B2135d2bE396'
 export const FACTORY_ADDRESS_97 = '0x8eED0642C17385297FdDF0B84EcA05C75112a83E'
-export const FACTORY_ADDRESS_25 = '0xd590cC180601AEcD6eeADD9B7f2B7611519544f4' // M
+export const FACTORY_ADDRESS_25 = '0x4000B3f4ad5A33f6c6015FFBEc3E9d6255e9a518' // COXI
 export const FACTORY_ADDRESS_338 = '0xd590cC180601AEcD6eeADD9B7f2B7611519544f4' // WRONG
 
+
+export const INIT_CODE_HASH_UNI_1 = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 export const INIT_CODE_HASH_56 = '0xbdc1513f01883597bcb8fd9b264667ad620715125e0d3339a1c6a2ffb80bee5d'
 export const INIT_CODE_HASH_97 = '0xe0bbdfcf9b856a3a04fbd17a1a9ce830f1de7d1d57760cd26a478120c69474b8'
-export const INIT_CODE_HASH_25 = '0x7ae6954210575e79ea2402d23bc6a59c4146a6e6296118aa8b99c747afec8acf' // M
+export const INIT_CODE_HASH_25 = '0x378bc11c6061f916de4910f5e7f30310318db12f7df2fee41c9e84c9cb84f12b' // COXI
 export const INIT_CODE_HASH_338 = '0x7ae6954210575e79ea2402d23bc6a59c4146a6e6296118aa8b99c747afec8acf' // WRONG
+
 
 
 export const getFactoryAddress = (chainId: number) => {
@@ -22,7 +26,8 @@ export const getFactoryAddress = (chainId: number) => {
         case 97:
             return FACTORY_ADDRESS_97;
         default:
-             throw Error('Coxinha SDK: getFactoryaddress: Invalid chainId')
+            return FACTORY_ADDRESS_56;
+            // return FACTORY_ADDRESS_UNI_1;
     }
 }
 
@@ -38,7 +43,8 @@ export const getInitCodeHash = (chainId: number) => {
         case 97:
             return INIT_CODE_HASH_97;
         default:
-             throw Error('Coxinha SDK: getInitCodeHash: Invalid chainId')
+            return INIT_CODE_HASH_56;
+            // return INIT_CODE_HASH_UNI_1;
     }
 }
 
